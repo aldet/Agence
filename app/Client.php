652de7f444e9c0af2c6d0bdbc2092efeecc57c9f
kossi <kossi@v12software.com>
2020-06-01 16:40:08 +0100

    Changes on client crud

--- app/Client.php
+++ app/Client.php
@@ -4,6 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Client
+ * @package App
+ * @property int $id
+ * @property string $nom_client
+ * @property string $num_permis
+ * @property string $prenom
+ * @property string $pays
+ * @property string $telephone
+ * @property string $code_postal
+ */
 class Client extends Model
 {
     protected $fillable = ['nom_client','num_permis','prenom','telephone','email','pays','code_postal','agence_id'];
